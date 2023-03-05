9464d692fa27df41ad992151e29d1fcce3b1045a
Шамиль Хайрулаев <sa.hairulaev@gmail.com>
2023-03-06 01:47:05 +0300

    Added csv importing products to database

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,10 +3,20 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use App\Form\ImportProductsFormType;
+use App\Repository\ProductRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeCrudActionEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Exception\ForbiddenActionException;
+use EasyCorp\Bundle\EasyAdminBundle\Exception\InsufficientEntityPermissionException;
+use EasyCorp\Bundle\EasyAdminBundle\Security\Permission;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -17,7 +27,53 @@ class ProductCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
-        return $actions;
+        $importCsvAction = Action::new('importCsv', 'Import', 'fas fa-file-excel')
+            ->displayAsLink()
+            ->setCssClass('btn btn-success')
+            ->linkToCrudAction('importCsv')
+            ->createAsGlobalAction();
+
+
+        return $actions
+            ->add(Crud::PAGE_INDEX, $importCsvAction);
+    }
+
+    public function importCsv(AdminContext $context, ProductRepository $productRepo)
+    {
+        $importForm = $this->createForm(ImportProductsFormType::class);
+        $importForm->handleRequest($context->getRequest());
+        if ($importForm->isSubmitted()) {
+            $formData = $importForm->getData();
+            /** @var UploadedFile $file */ $file = $formData['csv_file'];
+            $csvLines = explode(PHP_EOL, $file->getContent());
+            $fullCsv = [];
+            foreach ($csvLines as $line) {
+                $fullCsv[] = str_getcsv($line);
+            }
+            unset($fullCsv[0]);
+            unset($fullCsv[array_key_last($fullCsv)]);
+            $flush = false;
+            foreach (array_filter($fullCsv) as $key => $line) {
+                if ($key === array_key_last($fullCsv))
+                    $flush = true;
+                $product = new Product();
+                $product->setBrand($line[0]);
+                $product->setName($line[1]);
+                $product->setArticleNumber($line[2]);
+                $product->setPrice((float) $line[3]);
+                $product->setTotalBalance((float) $line[4]);
+                $product->setMeasurementUnit($line[5]);
+                $product->setAdditionalPrice((float) $line[6]);
+                $product->setImageLink($line[7]);
+                $product->setTechnicalDescription($line[8]);
+                $product->setUsed($line[9] === 'новая' ? 1 : 0);
+                $productRepo->save($product, $flush);
+            }
+            dd(123);
+        }
+        return $this->render('admin/product/import_csv.html.twig', [
+            'form' => $importForm
+        ]);
     }
 
     /*
---
+++ src/Form/ImportProductsFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Форма для импорта продуктов в БД из csv файла
+ */
+class ImportProductsFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('csv_file', FileType::class)
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}

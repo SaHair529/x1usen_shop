20f1bb40ae8b428be04f7b559f905983e0ffd266
Шамиль Хайрулаев <sa.hairulaev@gmail.com>
2023-03-08 12:00:03 +0300

    Added CsvProductImporter.php for importing products

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -4,7 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use App\Form\ImportProductsFormType;
-use App\Repository\ProductRepository;
+use App\Service\CsvProductImporter;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -38,38 +38,19 @@ class ProductCrudController extends AbstractCrudController
             ->add(Crud::PAGE_INDEX, $importCsvAction);
     }
 
-    public function importCsv(AdminContext $context, ProductRepository $productRepo)
+    public function importCsv(AdminContext $context, CsvProductImporter $csvImporter)
     {
         $importForm = $this->createForm(ImportProductsFormType::class);
         $importForm->handleRequest($context->getRequest());
         if ($importForm->isSubmitted()) {
             $formData = $importForm->getData();
             /** @var UploadedFile $file */ $file = $formData['csv_file'];
-            $csvLines = explode(PHP_EOL, $file->getContent());
-            $fullCsv = [];
-            foreach ($csvLines as $line) {
-                $fullCsv[] = str_getcsv($line);
+            if ($file->getMimeType() !== 'text/csv') {
+                dd('Неверный формат файла'); # todo добавить норм валидацию
             }
-            unset($fullCsv[0]);
-            unset($fullCsv[array_key_last($fullCsv)]);
-            $flush = false;
-            foreach (array_filter($fullCsv) as $key => $line) {
-                if ($key === array_key_last($fullCsv))
-                    $flush = true;
-                $product = new Product();
-                $product->setBrand($line[0]);
-                $product->setName($line[1]);
-                $product->setArticleNumber($line[2]);
-                $product->setPrice((float) $line[3]);
-                $product->setTotalBalance((float) $line[4]);
-                $product->setMeasurementUnit($line[5]);
-                $product->setAdditionalPrice((float) $line[6]);
-                $product->setImageLink($line[7]);
-                $product->setTechnicalDescription($line[8]);
-                $product->setUsed($line[9] === 'новая' ? 1 : 0);
-                $productRepo->save($product, $flush);
-            }
-            dd(123);
+            $csvImporter->importProducts($file);
+
+            return $this->redirectToRoute('admin');
         }
         return $this->render('admin/product/import_csv.html.twig', [
             'form' => $importForm

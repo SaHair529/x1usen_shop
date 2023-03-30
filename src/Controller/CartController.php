ca65aae10bdebbffb72429fb078f99a8fdce4259
Шамиль Хайрулаев <sa.hairulaev@gmail.com>
2023-03-31 01:33:40 +0300

    Добавил наброски логики работы с корзиной (Отображение продуктов карзины и их удаление)

---
+++ src/Controller/CartController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Cart;
+use App\Entity\User;
+use App\Repository\CartRepository;
+use App\Repository\ProductRepository;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/cart')]
+class CartController extends AbstractController
+{
+    /**
+     * @throws Exception
+     */
+    #[Route('/items', name: 'cart_items')]
+    public function index(): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        $cartItems = $user->getCart()->getProducts();
+
+        return $this->render('cart/index.html.twig', [
+            'cart_items' => $cartItems->getIterator()
+        ]);
+    }
+
+    /** @noinspection PhpPossiblePolymorphicInvocationInspection */
+    #[Route('/remove_item', name: 'cart_remove_item', methods: 'GET')]
+    public function removeItem(Request $req, ProductRepository $productRep, CartRepository $cartRep): Response
+    {
+        $itemId = $req->query->get('item_id');
+        if (!is_null($itemId) && !is_null($item = $productRep->findOneBy(['id'=>$itemId]))) {
+            /** @var Cart $cart */
+            $cart = $this->getUser()->getCart();
+            $cart->removeProduct($item);
+            $cartRep->save($cart, true);
+        }
+
+        return $this->redirectToRoute('cart_items');
+    }
+}

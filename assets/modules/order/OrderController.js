63247183377ece6ef43803cf138cc748c9e08539
Шамиль Хайрулаев <sa.hairulaev@gmail.com>
2023-05-29 01:40:32 +0300

    Добавил удаление оповещений смены статуса заказа при переходе на страницу заказов

--- assets/modules/order/OrderController.js
+++ assets/modules/order/OrderController.js
@@ -1,9 +1,11 @@
 import HTMLElements from "./HTMLElements";
 import ResponseHandler from "./ResponseHandler";
+import Routes from "../Routes";
 
 export default class OrderController {
     static init() {
         this.orderCardPressHandle()
+        this.clearOrderNotifications()
     }
 
     // event handles -----------------------
@@ -21,11 +23,34 @@ export default class OrderController {
     }
     // ________________________________________
 
-    // event handles actions --------------------
+    // actions --------------------
     static showProductInfoModal(productHref) {
         fetch(productHref).then(resp => {
             ResponseHandler.handleShowProductInfoModalResponse(resp)
         })
     }
+
+    static clearOrderNotifications() {
+        if (!window.location.href.includes('/order/my_orders')) {
+            return
+        }
+
+        const orderIdsWithNotifications = []
+        const ordersWithNotifications = document.querySelectorAll('.order-with-notifications')
+        for (let i = 0; i < ordersWithNotifications.length; i++) {
+            orderIdsWithNotifications.push(ordersWithNotifications[i].dataset.orderId)
+        }
+
+        if (orderIdsWithNotifications.length > 0) {
+            fetch(Routes.NotificationsController.clear_order_notifications, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({order_ids_with_notifications: orderIdsWithNotifications})
+            })
+        }
+    }
+
     // __________________________________________
 }

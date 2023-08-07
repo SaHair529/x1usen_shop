f85f972beb3577e45b950beef376aa73ae8ca1db
Шамиль Хайрулаев <sa.hairulaev@gmail.com>
2023-08-07 17:18:21 +0300

    Checkpoint. Добавил Запрос на калькулятор стоимости и сроков перевозки (Есть TODO)

--- src/Service/ThirdParty/Dellin/DellinApi.php
+++ src/Service/ThirdParty/Dellin/DellinApi.php
@@ -37,6 +37,62 @@ class DellinApi
         ]);
     }
 
+    /**
+     * Запрос на калькулятор стоимости и сроков перевозки
+     * https://dev.dellin.ru/api/calculation/calculator/#_header14
+     */
+    public function requestCostAndDeliveryTimeCalculator(string $produceDate,
+                                                         string $derivalAddress,
+                                                         string $arrivalAddress,
+                                                         string $cargoMaxLength,
+                                                         string $cargoMaxWidth,
+                                                         string $cargoMaxHeight,
+                                                         string $cargoWeight,
+                                                         string $cargoTotalWeight,
+                                                         string $cargoTotalVolume)
+    {
+        $this->client->request('POST', "{$_ENV['DELLIN_API_DOMAIN']}/v2/calculator.json", [
+            'appkey' => $_ENV['DELLIN_APP_KEY'],
+            'sessionID' => $this->sessionId,
+            'delivery' => [
+                'deliveryType' => [
+                    'type' => 'auto'
+                ],
+                'derival' => [
+                    'produceDate' => $produceDate,
+                    'variant' => 'address',
+                    'address' => [
+                        'search' => $derivalAddress
+                    ],
+                    'time' => [ # todo заменить тестовое время на реальное и узнать подробнее об этом параметре
+                        'worktimeStart' => '12:00',
+                        'worktimeEnd' => '21:00'
+                    ]
+                ],
+                'arrival' => [
+                    'variant' => 'address',
+                    'address' => [
+                        'search' => $arrivalAddress
+                    ],
+                    'time' => [ # todo заменить тестовое время на реальное и узнать подробнее об этом параметре
+                        'worktimeStart' => '16:00',
+                        'worktimeEnd' => '16:30'
+                    ]
+                ]
+            ],
+            'cargo' => [
+                'quantity' => '1',
+                'length' => $cargoMaxLength,
+                'width' => $cargoMaxWidth,
+                'height' => $cargoMaxHeight,
+                'weight' => $cargoWeight,
+                'totalVolume' => $cargoTotalVolume,
+                'totalWeight' => $cargoTotalWeight,
+                'freightUID' => '0x982400215e7024d411e1e844ef594aad'
+            ]
+        ]);
+    }
+
     private function setSessionId(): void
     {
         $cachedSessionId = $this->memcached->getItem(self::MC_SESSION_ID_KEY);

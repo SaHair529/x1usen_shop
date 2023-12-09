f0cbff0ae2fe86cade487976575ed7211145b0d8
Шамиль Хайрулаев <sa.hairulaev@gmail.com>
2023-12-09 11:14:10 +0300

    Добавил добавление всех доступов по апи при регистрации

--- src/Service/ThirdParty/Abcp/AbcpBackDoor.php
+++ src/Service/ThirdParty/Abcp/AbcpBackDoor.php
@@ -13,6 +13,7 @@ class AbcpBackDoor
 {
     private const LOGIN_URL = 'https://cp.abcp.ru/';
     private const ADD_IP_URL = 'https://cp.abcp.ru/ajaxRoute/customers/addCaIp';
+    private const ADD_PERMISSIONS_URL = 'https://cp.abcp.ru/ajaxRoute/customers/ApiPermissions/update';
 
     /**
      * Вход в систему ABCP для получения authtoken из cookies
@@ -49,6 +50,40 @@ class AbcpBackDoor
         ];
 
         $httpClient->request('POST', self::ADD_IP_URL, [
+            'body' => $requestData,
+            'headers' => $headers
+        ]);
+    }
+
+    public static function addAllPermissions(string $customerId, string $cookies)
+    {
+        $httpClient = HttpClient::create();
+
+        $requestData = [
+            'customerId' => $customerId,
+            'permissions[1]' => 1,
+            'permissions[2]' => 1,
+            'permissions[4]' => 1,
+            'permissions[5]' => 1,
+            'permissions[6]' => 1,
+            'permissions[16]' => 1,
+            'permissions[25]' => 1,
+            'permissions[26]' => 1,
+            'permissions[27]' => 1,
+            'permissions[28]' => 1,
+            'permissions[29]' => 1,
+            'permissions[32]' => 1,
+            'permissions[1007]' => 1,
+            'checkWoPrice' => 0,
+            'checkWoAvailability' => 0,
+            'allowOnlineRequest' => 1
+        ];
+
+        $headers = [
+            'Cookie' => $cookies
+        ];
+
+        $httpClient->request('POST', self::ADD_PERMISSIONS_URL, [
             'body' => $requestData,
             'headers' => $headers
         ]);
